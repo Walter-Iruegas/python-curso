ea93198dc793f92cc4239a81a5faf4b80d5c8d3f
Walter <walter.iruegas@outlook.com>
2024-01-17 10:01:31 -0600

    ficheros de Leccion 2

---
+++ SentenciaIfElse.py
@@ -0,0 +1,10 @@
+condicion = True
+
+#usar tabuladores para tner errores de indetacion o uno o mas espacios
+#if lo que espera es un valor, si esn valor vacio o cadena vacia es false
+if condicion == True:
+    print('Condicion verdadera')
+elif condicion == False:
+    print('Condicion Falsa')
+else:
+    print('Condicion no reconocida')
