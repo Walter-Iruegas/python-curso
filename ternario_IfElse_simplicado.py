bc1d94186f48a10c2c7a3901234b6fe39d7a3482
Walter <walter.iruegas@outlook.com>
2024-01-18 10:50:34 -0600

    Leccion 2 python

---
+++ ternario_IfElse_simplicado.py
@@ -0,0 +1,13 @@
+condicion = False
+
+#if condicion:
+ #   print('Condicion verdadera')
+#else:
+ #   print('Condiciopn falsa')
+
+#operador ternario
+
+#unicamente se recomeinda si el codigo es muy pequeño
+#sintaxis simplicadas
+print('Condicion Vardadera') if condicion else print('Condicion Falsa')
+
