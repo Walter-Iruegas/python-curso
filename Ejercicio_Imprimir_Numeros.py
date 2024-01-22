987f061ef69526a20e55eb34fc20dab7d6696f6e
Walter <walter.iruegas@outlook.com>
2024-01-22 13:54:18 -0600

    Leccion 3 python

---
+++ Ejercicio_Imprimir_Numeros.py
@@ -0,0 +1,10 @@
+#Imprimir numeros enteros del 0 al 5 utilizando un cilco While
+
+contador = 0
+maximo = 5
+while contador <= maximo:
+    print(contador)
+    #contador = contador + 1
+    contador += 1
+else:
+    print("Fin del Ciclo")
